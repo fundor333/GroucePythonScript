68eb14d2f9a5362e9c37a0440a5cc76cafc9ec46
Fundor333 <fundor333@gmail.com>
2016-04-09 08:57:40 +0200

    Adding a Starting point

---
+++ groucepython.py
@@ -0,0 +1,10 @@
+import urllib, hashlib
+
+# Set your variables here
+email = "someone@somewhere.com"
+default = "http://www.example.com/default.jpg"
+size = 40
+
+# construct the url
+gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
