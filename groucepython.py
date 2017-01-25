91ef23ba8fccf45c0948450b6228d496edf9b215
Fundor333 <fundor333@gmail.com>
2017-01-25 13:11:37 +0100

    Ended development

--- groucepython.py
+++ groucepython.py
@@ -1,21 +1,33 @@
 import os
-import urllib, hashlib
+import re
+import hashlib
 import subprocess
-from git import Repo
+import urllib
+import time
 
 # Set your variables here
-email = "someone@somewhere.com"
-default = "http://www.example.com/default.jpg"
+directory = ".git/avatar/"
 size = 40
 
-join = os.path.join
-repo = Repo(self.rorepo.working_tree_dir)
-assert not repo.bare
-commit_list = list(repo.iter_commits('master'))
+if (os.path.exists('.git')):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-# construct the url
-gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+    process = subprocess.Popen('''git log --pretty=format:"%ae,%an"'''.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    if (error == None):
+        dictionary = {}
+        for l in output.split('\n'):
+            email, name = re.findall(r'"(.*?),(.*?)"', l)[0]
+            dictionary[email] = name
 
-subprocess.call(["gource","-1024x768","--seconds-per-day 1","--stop-position 1.0", "--highlight-all-users", "--hide-filenames","--output-framerate 60","--output-ppm-stream output.ppm", "--user-image-dir .git/avatar/","--title 'Magic'","--hide dirnames,filenames,usernames"])
-subprocess.call(["ffmpeg", "-y", "-r 60", "-f image2pipe", "-vcodec ppm", "-i output.ppm" ,"-vcodec wmv1", "-r 60", "-qscale 0" ,"out.wmv"])
+        for email in dictionary.keys():
+            name = dictionary[email]
+            gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+            gravatar_url += urllib.urlencode({'d': "http://www.example.com/default.png", 's': str(size)})
+            gravatar_name = directory + name + '.png'
+            print("Getting image for " + name + " from url " + gravatar_url)
+            urllib.urlretrieve(gravatar_url, gravatar_name)
+
+        subprocess.call(["gource","-1024x768","--seconds-per-day 1","--stop-position 1.0", "--highlight-all-users", "--hide-filenames","--output-framerate 60","--output-ppm-stream output.ppm", "--user-image-dir .git/avatar/","--title 'Magic'","--hide dirnames,filenames,usernames"],shell=True)
+        subprocess.call(["ffmpeg", "-y", "-r 60", "-f image2pipe", "-vcodec ppm", "-i output.ppm" ,"-vcodec wmv1", "-r 60", "-qscale 0" ,"out.wmv"],shell=True)
